160bba305122cb1da9805380dcb7330f02e071a2
Dmitry Avvakumov <avvakumov@east-media.ru>
2016-03-18 18:03:39 +0500

    version 2.0

---
+++ classes/DMNetworkHelper.h
@@ -0,0 +1,18 @@
+//
+//  DMNetworkHelper.h
+//  DMNetworkHelper
+//
+//  Created by Avvakumov Dmitry on 18.03.16.
+//  Copyright © 2016 Dmitry Avvakumov. All rights reserved.
+//
+
+#ifndef DMNetworkHelper_h
+#define DMNetworkHelper_h
+
+#import "DMNetworkHelperManager.h"
+
+#import "DMNetworkHelperRequestTask.h"
+#import "DMNetworkHelperDownloadTask.h"
+#import "DMNetworkHelperUploadTask.h"
+
+#endif /* DMNetworkHelper_h */
